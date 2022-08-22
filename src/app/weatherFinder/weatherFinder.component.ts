c1013a28398396f4bf7ca79ca92f8234c2b658f0
raghadalju96 <raghad.alju96@gmail.com>
2022-08-22 08:32:46 +0300

    small chanages

--- src/app/weatherFinder/weatherFinder.component.ts
+++ src/app/weatherFinder/weatherFinder.component.ts
@@ -25,52 +25,97 @@ interface ApiResponse {
 export class WeatherFinder implements OnInit {
   constructor(private httpClient: HttpClient) {}
   CityName = "";
-  cityWeather = "";
-  cityWind = "";
-  cityHumidity = "";
-  cityDetsils: {};
+  // cityWeather = "";
+  // cityWind = "";
+  // cityHumidity = "";
+  // cityDetsils: {};
   isCold: boolean = false;
   isHot: boolean = false;
   showNoResult: boolean = false;
   showDetails: boolean = false;
+
+  totalPages: any = [];
+  CityWeatherDetails: CityWeather[] = [];
+
   ngOnInit(): void {}
+  // search() {
+  //   this.showNoResult = true;
+  //   this.showDetails = false;
+  //   if (this.CityName) {
+  //     this.httpClient
+  //       .get<Array<CityWeather>>(
+  //         `https://jsonmock.hackerrank.com/api/weather?name=${this.CityName}`
+  //       )
+  //       .subscribe((res: any) => {
+  //         console.log(res.data);
+  //         this.cityDetsils = res.data;
+
+  //         if (res.data.length >= 2 || res.data.length == 0) {
+  //           this.showNoResult = true;
+  //           this.showDetails = false;
+  //         } else {
+  //           this.showNoResult = false;
+  //           this.showDetails = true;
+  //           this.cityWeather = this.cityDetsils[0].weather;
+  //           this.cityWind = this.cityDetsils[0].status[0];
+  //           this.cityHumidity = this.cityDetsils[0].status[1];
+  //           let m = this.cityDetsils[0].weather;
+
+  //           let temp = m.split(" ");
+  //           console.log(temp);
+
+  //           if (temp[0] <= 20) {
+  //             console.log(temp[0]);
+  //             this.isCold = true;
+  //             this.isHot = false;
+  //           } else {
+  //             this.isHot = true;
+  //             this.isCold = false;
+  //           }
+  //         }
+  //       });
+  //   }
+  // }
 
   search() {
     this.showNoResult = true;
     this.showDetails = false;
     if (this.CityName) {
-      this.httpClient
-        .get<Array<CityWeather>>(
-          `https://jsonmock.hackerrank.com/api/weather?name=${this.CityName}`
-        )
-        .subscribe((res: any) => {
-          console.log(res.data);
-          this.cityDetsils = res.data;
+      this.getCityDetails(this.CityName).subscribe((res) => {
+        console.log(res);
 
-          if (res.data.length >= 2 || res.data.length == 0) {
-            this.showNoResult = true;
-            this.showDetails = false;
+        if (res.data.length >= 2 || res.data.length == 0) {
+          this.showNoResult = true;
+          this.showDetails = false;
+        } else {
+          this.totalPages = res.total_pages;
+          console.log(this.totalPages);
+          this.CityWeatherDetails = res.data;
+          console.log(this.CityWeatherDetails);
+          this.showDetails = true;
+          this.showNoResult = false;
+
+          let m = this.CityWeatherDetails[0].weather;
+
+          let temp = m.split(" ");
+          console.log(typeof temp);
+
+          if (temp[0] <= 20) {
+            console.log(temp[0]);
+            this.isCold = true;
+            this.isHot = false;
           } else {
-            this.showNoResult = false;
-            this.showDetails = true;
-            this.cityWeather = this.cityDetsils[0].weather;
-            this.cityWind = this.cityDetsils[0].status[0];
-            this.cityHumidity = this.cityDetsils[0].status[1];
-            let m = this.cityDetsils[0].weather;
-
-            let temp = m.split(" ");
-            console.log(temp);
-
-            if (temp[0] <= 20) {
-              console.log(temp[0]);
-              this.isCold = true;
-              this.isHot = false;
-            } else {
-              this.isHot = true;
-              this.isCold = false;
-            }
+            this.isHot = true;
+            this.isCold = false;
           }
-        });
+        }
+      });
     }
+  }
+
+  getCityDetails(CityName: any): Observable<ApiResponse> {
+    const URL = "https://jsonmock.hackerrank.com/api/weather?name=" + CityName;
+
+    return this.httpClient.get<ApiResponse>(URL).pipe(map((data) => data));
   }
 }
